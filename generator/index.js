a0241ad888bc4766c1c0ad6a215283b434e8ac21
zhoushuo <will_shuo@hotmail.com>
2019-04-23 14:43:46 +0800

    init

--- generator/index.js
+++ generator/index.js
@@ -3,20 +3,19 @@ function addDependencies (api) {
     dependencies: {
       "axios": "^0.18.0",
       "babel-polyfill": "^6.26.0",
+      "vuex": '^3.0.1'
     },
     devDependencies: {
       "qs": "^6.5.2",
       "style-resources-loader": "^1.2.1",
+      "element-ui": "^2.7.2",
+      "vue-cli-plugin-element": "^1.0.0",
+      "babel-plugin-component": "^1.1.1"
     }
   })
 }
 
 function renderFiles (api, opts) {
-  // Render files if we're replacing
-  const fs = require('fs');
-  const routerPath = api.resolve('./src/router.js');
-  opts.router = fs.existsSync(routerPath);
-
   // 配置
   if (opts.replaceTemplates) {
     api.render(files => {
@@ -25,32 +24,6 @@ function renderFiles (api, opts) {
         .forEach(name => delete files[name])
     })
     api.render('./templates/default');
-    if (opts.router) {
-      // 替换掉路由文件
-      api.render('./templates/router')
-    }
-  }
-
-  // 安装 vuex
-  if (opts.vuex) {
-    api.extendPackage({
-      dependencies: {
-        vuex: '^3.0.1'
-      }
-    });
-    api.render('./template/vuex');
-  }
-
-  // 安装 element-ui 库
-  if (opts.elementUI) {
-    api.extendPackage({
-      devDependencies: {
-        "element-ui": "^2.7.2",
-        "vue-cli-plugin-element": "^1.0.0",
-        "babel-plugin-component": "^1.1.1"
-      }
-    });
-    api.render('./templates/el')
   }
 
   // 配置文件
--- generator/templates/default/src/main.js
+++ generator/templates/default/src/main.js
@@ -1,15 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './stores'
 import 'babel-polyfill'
-<%_ if (!options.vuex) { _%>
-import store from './store'
-<%_ } _%>
 
-<%_ if (!options.elementUI) { _%>
 // 导入element
-import './element.js'
-<%_ } _%>
+import './plugins/element.js'
 
 // 导入插件
 import plugins from './plugins'
@@ -22,8 +18,6 @@ Vue.mixin(mixins)
 
 new Vue({
   router,
-  <%_ if (!options.vuex) { _%>
   store,
-  <%_ } _%>
   render: h => h(App)
 }).$mount('#app')
